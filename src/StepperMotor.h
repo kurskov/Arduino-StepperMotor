de30a134ce7b9a1f40942ed2d4a923b73a5577f1
kurskov <dmitrii@kurskov.ru>
2023-01-20 15:00:40 +0300

    Header file and roadmap

--- src/StepperMotor.h
+++ src/StepperMotor.h
@@ -10,19 +10,70 @@
 #ifndef _DK_StepperMotor_h
 #define _DK_StepperMotor_h
 
-#if defined(ARDUINO) && ARDUINO >= 100
-    #include <Arduino.h>
-#else
-    #include <WProgram.h>
-#endif
+#include "Arduino.h"
 
 class StepperMotor {
     public:
-        StepperMotor();
+        /* Constructors */
+
+        // Step-dir driver
+        StepperMotor(uint16_t steps; uint8_t step_pin; uint8_t dir_pin);
+        // Step-dir driver with enable pin
+        StepperMotor(uint16_t steps; uint8_t step_pin; uint8_t dir_pin; uint8_t enable_pin);
+        // Four pins driver
+        StepperMotor(uint16_t steps; uint8_t pin_A; uint8_t pin_B; uint8_t pin_C; uint8_t pin_D);
+
+
+        /* Motor settings */
+
+        // Invert direction pin (default is false)
+        void invertDirPin();
+        // Invert enable pin (default is false)
+        void invertEnablePin();
+        // Set motor max speed (steps per second) 
+        // For half pitch mode step is half of full step
+        // Default value is 300 steps per second.
+        void setMaxSpeed(uint16_t max_speed);
+        // Set/unset half pitch mode (default is false)
+        void setHalfPitch(bool half_pitch);
+        // Get half pitch mode value
+        bool getHalfPitch();
+
+
+        /* Motion parameters */
+
+        // Set speed for motions (steps per second)
+        void setSpeed(uint16_t speed);
+        // Set speed for motions (degrees per second)
+        void setSpeedDeg(uint8_t speed);
+        // Set steps value for motion
+        void setTarget(int16_t steps);
+        // Set degrees value for motion
+        void setTargetDeg(int16_t degrees);
+
+
+        /* Basic functions */
+
+        // Do one step
+        bool step();
+        // Enable motor
+        void enable();
+        // Disable motor
+        void disable();
 
 
     private:
+        uint16_t _steps;
+        uint8_t _step_pin = 0;
+        uint8_t _enable_pin = 0;
+        uint8_t _pin_A, _pin_B, _pin_C, _pin_D;
 
+        bool _invert_dir_pin = false;
+        bool _invert_enable_pin = false;
+        bool _halfPitch = false;
+        uint16_t _max_speed = 300;
+
+        uint16_t step_period;
 };
 
 #endif // _DK_StepperMotor_h
